e42f171f442c96bec49d5e8230da15c7297c4f8f
Bethany Fitton <bethanyy.fitton@gmail.com>
2020-09-26 20:57:35 +0100

    Updates

---
+++ precache-manifest.a2f40a57bfbd15a670694ba21d5bf3ab.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e17f9fc31ead3150b4d0a9bd7fc7005d",
+    "url": "/Calculator/index.html"
+  },
+  {
+    "revision": "fb0f1ac485ae66c81122",
+    "url": "/Calculator/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "72b260f8b645c005abb7",
+    "url": "/Calculator/static/css/main.4df95673.chunk.css"
+  },
+  {
+    "revision": "fb0f1ac485ae66c81122",
+    "url": "/Calculator/static/js/2.c7ebd3e1.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Calculator/static/js/2.c7ebd3e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72b260f8b645c005abb7",
+    "url": "/Calculator/static/js/main.15d3075a.chunk.js"
+  },
+  {
+    "revision": "0a79fa1d2af7875576c8",
+    "url": "/Calculator/static/js/runtime-main.8483c221.js"
+  }
+]);
